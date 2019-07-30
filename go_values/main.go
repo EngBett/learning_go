33f9e3e028948cd38b1f73383b1c05090558bb33
Bett <bettthetimetraveller@gmail.com>
2019-07-30 10:01:48 +0300

    go values 2

--- go_values/main.go
+++ go_values/main.go
@@ -7,5 +7,20 @@ import (
 
 func main() {
 	fmt.Println("Hello Go")
+
+	//mathematical expressions
+	fmt.Println("1+1 is equal to", 1 + 1)
+	fmt.Println("1-1 is equal to", 1 - 1)
+	fmt.Println("1*1 is equal to", 1 * 1)
+	fmt.Println("1/1 is equal to", 1 / 1)
+	fmt.Println("1%1 is equal to", 1 % 1)
+
+	//floating points
+	fmt.Println(1.15 - 0.0275)
+
+	//booleans
+	fmt.Println(5 == 5)
+	fmt.Println(6 == 5)
+
 	fmt.Println("The time is :", time.Now())
 }
