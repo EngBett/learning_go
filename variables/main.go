5a517edb50bfdaecc0207d93b3915fff78d1ebd3
Bett <bettthetimetraveller@gmail.com>
2019-07-30 10:11:21 +0300

    variables

---
+++ variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var my_items int = 100
+
+	fmt.Println(my_items)
+
+	//var price1, price2 float64 = 90.02,20.05
+	price1, price2 := 90.02,20.05
+
+	var s = "Hello Go"
+
+	fmt.Println(price1, price2)
+	fmt.Println(s)
+
+}
