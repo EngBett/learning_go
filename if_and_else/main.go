233e441e3d0e009b247920a434e5160c84bc8b7f
Bett <bettthetimetraveller@gmail.com>
2019-07-30 10:46:39 +0300

    if and else conditionals

---
+++ if_and_else/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	i:=10
+
+	if i<10{
+		fmt.Println("Less than 10")
+	}else if i==10{
+		fmt.Println("Equals to 10")
+	}else{
+		fmt.Println("Greater than 10")
+	}
+}
