4b92dd9b4c3c80c2898f0c4759031c33fac1e0f1
EngBett <bettthetimetraveller@gmail.com>
2019-10-05 12:13:23 +0300

    Go Slices

--- slices/main.go
+++ slices/main.go
@@ -7,5 +7,52 @@ func main() {
 	s[1] = 3
 	s[0] = 5
 	s[2] = 8
+
+	//appending
+	s = append(s, 15, 18, 67)
+
+	//length of slice
+	fmt.Println(len(s))
+
+	//part of slice
+	fmt.Println(s[:3])
+
+	fmt.Println(s[3:])
+
+	test := make([]int, 3)
+
+	fmt.Println(test)
+
+	// concise slice definition
+	t := []int{100, 200, 300}
+
+	fmt.Println(t)
+
+	//changing values
+
+	/*fmt.Println(s)
+	x:=s
+	x[0]=200
+	fmt.Println(x)
+	fmt.Println(s)*/
+
+	//use copy to prevent from changing both x and s
+	x := make([]int, len(s))
+	copy(x, s)
+	x[0] = 500
+	fmt.Println(x)
 	fmt.Println(s)
+
+	//2d slices
+	ss := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		ss[i] = make([]int, innerLen)
+		for j := 0; j < len(ss[i]); j++ {
+			ss[i][j] = i + j
+		}
+	}
+
+	fmt.Println(ss)
+
 }
